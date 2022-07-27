e863c085dac320e2c6fdc0185c80b917f7f95b84
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-27 15:26:05 +0700

    Typography

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,48 @@
+import { Typography } from "@mui/material";
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">H1 Heading</Typography>
+      <Typography variant="h2">H2 Heading</Typography>
+      <Typography variant="h3">H3 Heading</Typography>
+      <Typography variant="h4">H4 Heading</Typography>
+      <Typography variant="h5">H5 Heading</Typography>
+      <Typography variant="h6">H6 Heading</Typography>
+
+      <hr />
+
+      <Typography variant="subtitle1">Sub Title 1</Typography>
+      <Typography variant="subtitle2">Sub Title 2</Typography>
+
+      <hr />
+
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga possimus
+        deleniti, pariatur obcaecati maxime molestiae eveniet dignissimos
+        eligendi officiis architecto numquam labore id nam repellendus, sunt
+        unde in, incidunt qui porro? Facere culpa quasi assumenda tempore
+        quibusdam? Beatae fugiat sed provident dignissimos quasi ad dolores
+        velit dolorem, sit est consectetur?
+      </Typography>
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed optio aut
+        saepe earum reiciendis. Nostrum officiis vel consequuntur! Amet quas,
+        nesciunt culpa nam eum sequi, accusamus ipsum doloribus, inventore enim
+        libero deserunt? Quibusdam quia commodi, incidunt dolore sed velit
+        eveniet nihil placeat, labore perferendis ab hic laboriosam sit tempora
+        consectetur.
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex delectus
+        laboriosam unde officiis exercitationem quod, odio accusantium eum
+        pariatur iure illo optio tenetur iusto assumenda porro odit. Temporibus
+        est ratione qui, perferendis ipsum laudantium. Et molestiae placeat,
+        exercitationem laboriosam, obcaecati totam perspiciatis aliquam animi
+        assumenda consequuntur unde sunt ducimus. Voluptatum.
+      </Typography>
+    </div>
+  );
+};
+
+export default MuiTypography;
