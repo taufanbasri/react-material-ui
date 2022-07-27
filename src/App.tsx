f4b124ba190e4dd9aa1535ab2a16663318d138aa
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-27 16:45:47 +0700

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import MuiButton from "./components/MuiButton";
 import MuiTypography from "./components/MuiTypography";
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography />
+      <MuiButton />
+      {/* <MuiTypography /> */}
     </div>
   );
 }
---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,68 @@
+import { Button, Stack, IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+
+const MuiButton = () => {
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="www.google.com/">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+        <Button variant="contained" color="warning">
+          warning
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="success">
+          success
+        </Button>
+      </Stack>
+
+      <Stack display={"block"} spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          medium
+        </Button>
+        <Button variant="contained" size="large">
+          large
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          disableRipple
+          onClick={() => alert("Button Clicked")}
+        >
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          Send
+        </Button>
+        <IconButton aria-label="send" color="success" size="small">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MuiButton;
