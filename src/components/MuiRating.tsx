2c51fbee5a32742b929d0304c47c0eb781a71b86
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-28 09:47:15 +0700

    Rating

---
+++ src/components/MuiRating.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Rating, Stack } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
+const MuiRating = () => {
+  const [value, setValue] = useState<number | null>(null);
+  const [valueReadOnly] = useState(3);
+
+  console.log({ value });
+
+  const handleChange = (
+    _event: React.ChangeEvent<{}>,
+    newValue: number | null
+  ) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Stack spacing={2}>
+      <Rating
+        value={value}
+        onChange={handleChange}
+        precision={0.5}
+        size="large"
+      />
+
+      <Rating
+        value={value}
+        onChange={handleChange}
+        precision={0.5}
+        size="large"
+        icon={<FavoriteIcon fontSize="inherit" color="error" />}
+        emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+      />
+
+      <Rating
+        readOnly
+        value={valueReadOnly}
+        precision={0.5}
+        size="large"
+        icon={<FavoriteIcon fontSize="inherit" color="error" />}
+        emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+      />
+    </Stack>
+  );
+};
+
+export default MuiRating;
