5918626f41091d7f1ee164781b8446062410ac5c
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-28 09:23:16 +0700

    Switch

---
+++ src/components/MuiSwitch.tsx
@@ -0,0 +1,24 @@
+import { Box, FormControlLabel, Switch } from "@mui/material";
+import React, { useState } from "react";
+
+const MuiSwitch = () => {
+  const [checked, setChecked] = useState(false);
+  console.log({checked});
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <Box>
+      <FormControlLabel
+        label="Dark Mode"
+        control={
+          <Switch checked={checked} onChange={handleChange} color="secondary" />
+        }
+      />
+    </Box>
+  );
+};
+
+export default MuiSwitch;
