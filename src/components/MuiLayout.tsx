487cfbb3b78f9d19869bda00a787c0c7036b1267
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-28 11:52:21 +0700

    Grid

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,37 +1,62 @@
-import { Box, Divider, Stack } from "@mui/material";
+import { Box, Divider, Grid, Stack } from "@mui/material";
 import React from "react";
 
 const MuiLayout = () => {
   return (
-    <Stack
-      sx={{ border: "1px solid" }}
-      direction="row"
-      spacing={2}
-      divider={<Divider orientation="vertical" flexItem />}
-    >
-      <Box
-        sx={{
-          backgroundColor: "primary.main",
-          color: "white",
-          height: "100px",
-          width: "100px",
-          padding: "16px",
-          "&:hover": {
-            backgroundColor: "primary.light",
-          },
-        }}
-      >
-        Lubna Tech Inovation
-      </Box>
+    <>
+      <Grid container my={4} rowSpacing={1} columnSpacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Box bgcolor={"primary.light"} p={2}>
+            Item 1
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box bgcolor={"primary.light"} p={2}>
+            Item 2
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box bgcolor={"primary.light"} p={2}>
+            Item 3
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box bgcolor={"primary.light"} p={2}>
+            Item 4
+          </Box>
+        </Grid>
+      </Grid>
 
-      <Box
-        display="flex"
-        height="100px"
-        width="100px"
-        bgcolor="success.light"
-        p={2}
-      ></Box>
-    </Stack>
+      <Stack
+        sx={{ border: "1px solid" }}
+        direction="row"
+        spacing={2}
+        divider={<Divider orientation="vertical" flexItem />}
+      >
+        <Box
+          sx={{
+            backgroundColor: "primary.main",
+            color: "white",
+            height: "100px",
+            width: "100px",
+            padding: "16px",
+            "&:hover": {
+              backgroundColor: "primary.light",
+            },
+          }}
+        >
+          Lubna Tech Inovation
+        </Box>
+
+        <Box
+          display="flex"
+          height="100px"
+          width="100px"
+          bgcolor="success.light"
+          p={2}
+        ></Box>
+      </Stack>
+    </>
   );
 };
 
