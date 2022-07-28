0c6128d23f9c026b71dbb7284ae83d5ea93c0d20
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-28 21:40:32 +0700

    Menu

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   AppBar,
   Button,
   IconButton,
+  Menu,
+  MenuItem,
   Stack,
   Toolbar,
   Typography,
 } from "@mui/material";
 
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const MuiNavbar = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -25,8 +39,37 @@ const MuiNavbar = () => {
           <Button color="inherit">Features</Button>
           <Button color="inherit">pricing</Button>
           <Button color="inherit">about</Button>
+          <Button
+            color="inherit"
+            id="resources-button"
+            onClick={handleClick}
+            aria-controls={open ? "resources-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            endIcon={<KeyboardArrowDownIcon />}
+          >
+            resources
+          </Button>
           <Button color="inherit">login</Button>
         </Stack>
+        <Menu
+          id="resources-menu"
+          anchorEl={anchorEl}
+          open={open}
+          MenuListProps={{ "aria-labelledby": "resources-button" }}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+        >
+          <MenuItem>Blog</MenuItem>
+          <MenuItem>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
