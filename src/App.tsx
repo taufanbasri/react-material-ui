eabbaead82fcc07741f24f41b8e2ef9ad53a8e57
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-07-29 17:36:24 +0700

    Bottom Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import "./App.css";
-import MuiDrawer from "./components/MuiDrawer";
+import MuiBottomNavigation from "./components/MuiBottomNavigation";
 import MuiNavbar from "./components/MuiNavbar";
-import MuiSpeedDial from "./components/MuiSpeedDial";
 
 function App() {
   return (
     <div className="App">
       <MuiNavbar />
-      <MuiSpeedDial />
+      <MuiBottomNavigation />
     </div>
   );
 }
